7f8f3002c35a8b95fd0532f10883e037d0986ff9
burakcokkececi363@outlook.com <burakcokkececi363@outlook.com>
2024-05-31 15:14:52 +0300

    ...

--- RealEstate_Dapper_Api/Hubs/SignalRHub.cs
+++ RealEstate_Dapper_Api/Hubs/SignalRHub.cs
@@ -4,5 +4,25 @@ namespace RealEstate_Dapper_Api.Hubs
 {
     public class SignalRHub : Hub
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public SignalRHub(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task SendCategoryCount()
+        {
+            var client = _httpClientFactory.CreateClient();
+
+
+            var responsemessage5 = await client.GetAsync("https://localhost:44350/api/Statistics/CategoryCount");
+            if (responsemessage5.IsSuccessStatusCode)//200 ile 299 arasında bir sayı dönerse true döneceğinden başarılı false dönerse başarısız
+            {
+                var jsondata5 = await responsemessage5.Content.ReadAsStringAsync();
+                await Clients.All.SendAsync("ReceiverCategoryCount",jsondata5);
+            }
+
+        }
     }
 }
