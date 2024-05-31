e71bc8112b99cff4f7a64b1e46e2f4c37f141149
burakcokkececi363@outlook.com <burakcokkececi363@outlook.com>
2024-05-31 16:29:35 +0300

    ...

---
+++ RealEstate_Dapper_Api/Enums/RolesType.cs
@@ -0,0 +1,11 @@
+﻿namespace RealEstate_Dapper_Api.RolesType
+{
+    public enum RolesType
+    {
+        Admin = 1,
+        Member = 2,
+        Manager = 3,
+        Visitor = 4,
+        Emplooyee = 5
+    }
+}
