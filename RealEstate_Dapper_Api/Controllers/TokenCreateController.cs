b968edf8dc19a817954426981ba8ec89e8b182f4
burakcokkececi363@outlook.com <burakcokkececi363@outlook.com>
2024-05-31 16:42:13 +0300

    ...

---
+++ RealEstate_Dapper_Api/Controllers/TokenCreateController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate_Dapper_Api.Tools;
+
+namespace RealEstate_Dapper_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenCreateController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> CreateToken(GetCheckAppUserViewModel getCheckAppUserViewModel)
+        {
+            var values = JwtTokenGenerator.GenerateToken(getCheckAppUserViewModel);
+            return Ok(values);  
+        }
+    }
+}
