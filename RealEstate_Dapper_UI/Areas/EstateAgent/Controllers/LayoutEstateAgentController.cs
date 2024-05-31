89471331fde02b7b32682c72190eb5085b9c2744
burakcokkececi363@outlook.com <burakcokkececi363@outlook.com>
2024-05-31 16:44:19 +0300

    ...

---
+++ RealEstate_Dapper_UI/Areas/EstateAgent/Controllers/LayoutEstateAgentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate_Dapper_UI.Areas.EstateAgent.Controllers
+{
+    public class LayoutEstateAgentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
