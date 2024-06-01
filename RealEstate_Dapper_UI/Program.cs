9727fa04432ea62d388ce842ef2c31bc0cea597d
burakcokkececi363@outlook.com <burakcokkececi363@outlook.com>
2024-06-01 18:55:48 +0300

    ...

--- RealEstate_Dapper_UI/Program.cs
+++ RealEstate_Dapper_UI/Program.cs
@@ -23,7 +23,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
 	name: "default",
-	pattern: "{controller=Home}/{action=Index}/{id?}");
+	pattern: "{controller=Default}/{action=Index}/{id?}");
 
 app.UseEndpoints(endpoints =>
 {
