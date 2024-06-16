41d93c4837187ad4d89529e1055ab70fce281ff4
burakcokkececi363@outlook.com <burakcokkececi363@outlook.com>
2024-06-16 12:38:05 +0300

    ...

--- RealEstate_Dapper_Api/Dtos/PopularLocationDtos/ResultPopularLocationDto.cs
+++ RealEstate_Dapper_Api/Dtos/PopularLocationDtos/ResultPopularLocationDto.cs
@@ -3,6 +3,7 @@
 	public class ResultPopularLocationDto
 	{
         public short LocationId { get; set; }
+        public int PropertyCount { get; set; }
         public string CityName { get; set; }
         public string ImageUrl { get; set; }
     }
